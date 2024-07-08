e4a7fd07a6b359671cb10ceffba2310b41f5f142
Ian Wegerle <24674138+ianweg01@users.noreply.github.com>
2024-07-08 16:26:03 +0200

    "Added 'cart' app to settings, updated models with get_absolute_url methods, modified templates to use get_absolute_url, added new URLs and views for product info and category listing"

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -11,6 +12,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+    def get_absolute_url(self):
+        return reverse("list-category", args=[self.slug])
+        
 class Product(models.Model):
     # FK
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE, null=True)
@@ -26,4 +30,8 @@ class Product(models.Model):
         
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("product-info", args=[self.slug])
+    
     
