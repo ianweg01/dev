a4ba2d00c3dc44316338b4a1cb898e263904c277
Ian Wegerle <24674138+ianweg01@users.noreply.github.com>
2024-07-11 14:53:08 +0200

    "Updated cart functionality: added update method to Cart class, modified cart-summary.html template, and implemented cart update view in views.py"

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -46,6 +46,15 @@ class Cart():
 
         self.session.modified = True
            
+           
+    def update(self, product, qty):
+        product_id = str(product)
+        product_quantity = qty
+        
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = product_quantity
+            
+        self.session.modified = True
             
         
     def __len__(self):
