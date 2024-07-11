6a85489f5c073afba6f40a5453a295c4bd13e60e
Ian Wegerle <24674138+ianweg01@users.noreply.github.com>
2024-07-11 09:20:18 +0200

    Adding cart

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 
 def cart_summary(request):
-    return render(request, 'cart/cart-summary.html')
+    cart = Cart(request)
+    
+    return render(request, 'cart/cart-summary.html', {'cart': cart})
     
 def cart_add(request):
     cart = Cart(request)
@@ -26,7 +28,19 @@ def cart_add(request):
         return response
     
 def cart_delete(request):
-    ...
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        cart.delete(product=product_id)
+        
+        cart_quantity = cart.__len__()
+        
+        cart_total = cart.get_total()
+        
+        response = JsonResponse({'qty': cart_quantity, 'total': cart_total})
+        
+        return response
+        
     
 def cart_update(request):
     ...
